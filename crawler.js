23d85913f6e6d4b1458bde40494014eaa3105568
Connor <chopcraft@uchicago.edu>
2018-11-13 11:33:00 -0600

    first push

--- crawler.js
+++ crawler.js
@@ -23,70 +23,14 @@ function getID(string) {
   return id;
 }
 
-/* || Work In Progress || */
-function testCrossOrg(id) {
-  var newurl = "https://scratch.mit.edu/site-api/projects/in/" + id + "/1/";
-  var request = new XMLHttpRequest();
-  request.open('GET', newurl);
-  request.send();
-
-  /* Handle response. */
-  request.onload = function() {
-    if(request.status != 200) {
-      cross_org = false;      
-      console.log(cross_org);
-      return;
-    }
-    console.log(cross_org);
-    console.log(request.response);
-  }
-}
-
-/* Requests html from a studio page and recursively checks other studio pages. */
-function crawl(id, page) {
-  
-  /* Prepare link and send request. */
-  var newurl = "https://scratch.mit.edu/site-api/projects/in/" + id + "/" + page + "/";
-  var request = new XMLHttpRequest();
-  request.open('GET', newurl);
-  request.send();
-
-  /* Handle response. */
-  request.onload = function() {
-    if(request.status != 200) {
-      transferFailed(page);
-      return;
-    }
-    var project = request.response;
-
-    collectLinks(project);
-    crawl(id, page + 1);
-  }
-
-  /* Handle error. */
-  request.onerror = function() {
-    clearReport();
-    linkError();
-  }
-}
-
-/* Logs unsuccessful transfer (generally intentional). */
-function transferFailed(page) {
-  console.log("XML transfer terminated on page " + page + ".");
-
-  if(page == 1) {
-    linkError();
-  }
-  else{
-    crawl_finished = true;
-    checkIfComplete();
-  }
-}
-
 /* Collects links to project pages from studio html and initiates JSON recovery. */
 function getRequestURL(id) {
-  var pre = "https://thawing-escarpment-43547.herokuapp.com/";
-  return pre + id;
+  console.log("here");
+
+  var url = "http://chord.cs.uchicago.edu/pdf_gen/" + id;
+  window.open(url, '_blank');
+  return;
+  //getJSON(url);
 }
 
 /* Request project jsons and initiate analysis. */
@@ -103,9 +47,6 @@ function getJSON(requestURL,process_function, args){
         return;
       }
       console.log(request.status);
-      var project = request.response;
-      args.unshift(project)
-      process_function.apply(null,args);
     }
     
 }
--- main.js
+++ main.js
@@ -1,198 +1,27 @@
-/* MAKE SURE OBJ'S AUTO INITIALIZE AT GRADE */
 
-/* Stores the grade reports. */
-var reports_list = [];
-/* Number of projects scanned so far. */
-var project_count = 0;
-/* Cross-origin request permissibility [UNUSED]*/
-var cross_org = true;
-/* Sets to true when next page returns 404. */
-var crawl_finished = false;
-/* Number of projects that meet requirements. */
-var passing_projects = 0;
-/* Number of projects that meet requirements and extensions */
-var complete_projects = 0;
 /* Grading object. */
-var gradeObj = null;
-
-var table = 0;
-
 var IS_LOADING = false;
 
-/*
-  SELECTION HTML
-*/
 
-/* Initializes html and initiates crawler. */
+/* Initiates PDF retrieval. */
 function buttonHandler() {
   if(IS_LOADING) {
     return;
   }
 
-  if(!gradeObj) {
-    unitError();
-    return;
-  }
-
   htmlInit();
-  globalInit();
-  document.getElementById('wait_time').innerHTML = "loading...";
-  IS_LOADING = true;
-
 	var inputURL = document.getElementById('inches_input').value;
-  var requestURL = getRequestURL(getID(inputURL));
-  getJSON(requestURL,analyze,[]);
+  var id = getID(inputURL);
+  if (id) {getRequestURL(id);}
 }
 
 /* Initializes global variables. */
-function globalInit() {
-  reports_list = [];
-  project_count = 0;
-  crawl_finished = false;
-  cross_org = true;
-  grade_reqs = {};
-  passing_projects = 0;
-  complete_projects = 0;
-}
+
 
 /* Initializes HTML elements. */
 function htmlInit() {
   document.getElementById('process_button').blur();
-  clearReport();
   noError();
-}
-
-function dropdownHandler() {
-  document.getElementById("unit_dropdown").classList.toggle("show");
-}
-
-$(document).ready(function(){
-  $('.units label').click(function() {
-    $(this).addClass('selected').siblings().removeClass('selected');
-  });
-});
-
-function drop_eventHandler() {
-  gradeObj = new GradeEvents();
-  console.log("Grading Events");
-}
-
-function drop_animationHandler() {
-  gradeObj = new GradeAnimation();
-  console.log("Grading Animation");
-}
-
-function drop_condloopsHandler() {
-  gradeObj = new GradeCondLoops();
-  console.log("Grading Conditional Loops");
-}
-
-function drop_onewaysyncp1Handler() {
-   gradeObj = new GradeOneWaySyncP1();
-   console.log("Grading One Way Sync Pt. 1");
-}
-
-function drop_onewaysyncp2Handler() {
-   gradeObj = new GradeOneWaySyncP2();
-   console.log("Grading One Way Sync Pt. 2");
-}
-
-function drop_decompbyseqHandler() {
-   gradeObj = new GradeDecompBySeq();
-   console.log("Grading Decomposition By Sequence");
-}
-
-window.onclick = function(event) {
-  if(event.target.matches('.dropdown_btn')) {
-    return;
-  }
-
-  var droplinks = document.getElementsByClassName("dropdown_menu");
-  [...droplinks].forEach(function(element) {
-    if(element.classList.contains('show')) {
-      element.classList.remove('show');
-    }
-  });
-}
-
-/*
-  DISPLAY RESULTS
-*/
-
-/* Prints a line of grading text. */
-function appendText(string_list) {
-  var tbi = document.createElement("div");
-  tbi.className = "dynamic";
-
-  string_list.forEach(function(sub_element) {
-    var newContent = document.createTextNode(sub_element);
-    tbi.appendChild(newContent);
-    var br = document.createElement("br");
-    tbi.appendChild(br);
-  });
-
-  tbi.style.width = 33 + "%";
-  tbi.style.fontSize = "15px";
-  tbi.style.display= 'inline-block';
-
-  var ai = document.getElementById("report");
-  document.body.insertBefore(tbi, ai);
-
-  table++;
-  if (table > 2) {
-    table = 0;
-  }
-}
-
-/* Prints a blank line. */
-function appendNewLine() {
-  var tbi = document.createElement("div");
-  tbi.className = "lines";
-  tbi.style.padding = "15px";
-
-  var ai = document.getElementById("report");
-  document.body.insertBefore(tbi, ai);
-}
-
-/* Prints out the contents of report_list as a
-   series of consecutive project reports. */
-function printReport() {
-  clearReport();
-
-  printHeader();
-
-  table = 0;
-  reports_list.forEach(function(element) {
-    appendText(element);
-    if (table == 0){
-      appendNewLine();
-    }
-  });
-  processComplete();
-}
-
-/* Clears all project reports from the page. */
-function clearReport() {
-  var removeables = document.getElementsByClassName('dynamic');
-  while(removeables[0]) {
-    removeables[0].remove();
-  }
-  var removeables = document.getElementsByClassName('lines');
-  while(removeables[0]) {
-    removeables[0].remove();
-  }
-}
-
-/* Prints color key.*/
-function printHeader() {
-  var processObj = document.getElementById('process_status');
-  processObj.style.visibility = 'visible';
-  processObj.innerHTML = "My Project:";
-}
-
-function hideHeader() {
-  var processObj = document.getElementById('process_status');
-  processObj.style.visibility = 'hidden';
 }
 
 /* 
@@ -206,19 +35,17 @@ function linkError() {
   processObj.innerHTML = "error: invalid link.";
   document.getElementById('wait_time').innerHTML = "";
   document.getElementById('process_status').style.visibility = 'red';
-  hideHeader();
   IS_LOADING = false;
 }
 
-function unitError() {
-  var processObj = document.getElementById('process_error');
-  processObj.style.visibility = 'visible';
-  processObj.style.color = "red";
-  processObj.innerHTML = "error: no unit selected.";
-  IS_LOADING = false;
-}
 
 function noError() {
-  document.getElementById('process_error').innerHTML = "";
-  document.getElementById('process_error').style.visibility = 'hidden';
+  var processObj = document.getElementById('process_error');
+  processObj.style.visibility = 'visible';
+  processObj.style.color = "#123D6A";
+  processObj.innerHTML = "Note: loading may take several minutes.";
+  document.getElementById('wait_time').innerHTML = "";
+  document.getElementById('process_status').style.visibility = 'red';
+  IS_LOADING = false;
+
 }
